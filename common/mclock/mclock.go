25e1916210de60834acd1e9287294798470ca356
sunyihoo <sunyihoo@gmail.com>
2025-02-24 23:30:36 +0800

    wip: finish mclock

--- common/mclock/mclock.go
+++ common/mclock/mclock.go
@@ -63,9 +63,63 @@ type Timer interface {
 // ChanTimer is a cancellable event created by NewTimer.
 type ChanTimer interface {
 	Timer
+
 	// The channel returned by C receives a value when the timer expires.
 	C() <-chan AbsTime
 	// Reset reschedules the timer with a new timeout.
 	// It should be invoked only on stopped or expired timers with drained channels.
 	Reset(time.Duration)
+}
+
+// System implements Clock using the system clock.
+type System struct{}
+
+// Now returns the current monotonic time.
+func (c System) Now() AbsTime {
+	return Now()
+}
+
+// Sleep blocks for the given duration.
+func (c System) Sleep(d time.Duration) {
+	time.Sleep(d)
+}
+
+// NewTimer creates a timer which can be rescheduled.
+func (c System) NewTimer(d time.Duration) ChanTimer {
+	ch := make(chan AbsTime, 1)
+	t := time.AfterFunc(d, func() {
+		// This send is non-blocking because that's how time.Timer
+		// behaves. It doesn't matter in the happy case, but does
+		// when Reset is misused.
+		select {
+		case ch <- c.Now():
+		default:
+		}
+	})
+	return &systemTimer{t, ch}
+}
+
+// After returns a channel which receives the current time after d has elapsed.
+func (c System) After(d time.Duration) <-chan AbsTime {
+	ch := make(chan AbsTime, 1)
+	time.AfterFunc(d, func() { ch <- c.Now() })
+	return ch
+}
+
+// AfterFunc runs f on a new goroutine after the duration has elapsed.
+func (c System) AfterFunc(d time.Duration, f func()) Timer {
+	return time.AfterFunc(d, f)
+}
+
+type systemTimer struct {
+	*time.Timer
+	ch <-chan AbsTime
+}
+
+func (st *systemTimer) Reset(d time.Duration) {
+	st.Timer.Reset(d)
+}
+
+func (st *systemTimer) C() <-chan AbsTime {
+	return st.ch
 }
