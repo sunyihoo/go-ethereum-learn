76866fcf6ce9a96d161935bac6e9248b4bde04aa
sunyihoo <sunyihoo@gmail.com>
2025-04-02 23:31:12 +0800

    notes & test: core rawdb.database

--- core/rawdb/database.go
+++ core/rawdb/database.go
@@ -33,6 +33,7 @@ import (
 )
 
 // freezerdb is a database wrapper that enables ancient chain segment freezing.
+// freezerdb 是一个数据库包装器，支持将古老的链段冻结。
 type freezerdb struct {
 	ethdb.KeyValueStore
 	*chainFreezer
@@ -42,12 +43,14 @@ type freezerdb struct {
 }
 
 // AncientDatadir returns the path of root ancient directory.
+// AncientDatadir 返回根祖先目录的路径。
 func (frdb *freezerdb) AncientDatadir() (string, error) {
 	return frdb.ancientRoot, nil
 }
 
 // Close implements io.Closer, closing both the fast key-value store as well as
 // the slow ancient tables.
+// Close 实现了 io.Closer，关闭快速键值存储和慢速古董表。
 func (frdb *freezerdb) Close() error {
 	var errs []error
 	if err := frdb.chainFreezer.Close(); err != nil {
@@ -65,11 +68,13 @@ func (frdb *freezerdb) Close() error {
 // Freeze is a helper method used for external testing to trigger and block until
 // a freeze cycle completes, without having to sleep for a minute to trigger the
 // automatic background run.
+// Freeze 是一个辅助方法，用于外部测试以触发并阻塞直到冻结周期完成，而不需要睡眠一分钟来触发自动后台运行。
 func (frdb *freezerdb) Freeze() error {
 	if frdb.readOnly {
 		return errReadOnly
 	}
 	// Trigger a freeze cycle and block until it's done
+	// 触发冻结周期并阻塞直到完成
 	trigger := make(chan struct{}, 1)
 	frdb.chainFreezer.trigger <- trigger
 	<-trigger
@@ -77,56 +82,67 @@ func (frdb *freezerdb) Freeze() error {
 }
 
 // nofreezedb is a database wrapper that disables freezer data retrievals.
+// nofreezedb 是一个数据库包装器，禁用冻结数据检索。
 type nofreezedb struct {
 	ethdb.KeyValueStore
 }
 
 // HasAncient returns an error as we don't have a backing chain freezer.
+// HasAncient 返回一个错误，因为我们没有支持链冻结器。
 func (db *nofreezedb) HasAncient(kind string, number uint64) (bool, error) {
 	return false, errNotSupported
 }
 
 // Ancient returns an error as we don't have a backing chain freezer.
+// Ancient 返回一个错误，因为我们没有支持链冻结器。
 func (db *nofreezedb) Ancient(kind string, number uint64) ([]byte, error) {
 	return nil, errNotSupported
 }
 
 // AncientRange returns an error as we don't have a backing chain freezer.
+// AncientRange 返回一个错误，因为我们没有支持链冻结器。
 func (db *nofreezedb) AncientRange(kind string, start, max, maxByteSize uint64) ([][]byte, error) {
 	return nil, errNotSupported
 }
 
 // Ancients returns an error as we don't have a backing chain freezer.
+// Ancients 返回一个错误，因为我们没有支持链冻结器。
 func (db *nofreezedb) Ancients() (uint64, error) {
 	return 0, errNotSupported
 }
 
 // Tail returns an error as we don't have a backing chain freezer.
+// Tail 返回一个错误，因为我们没有支持链冻结器。
 func (db *nofreezedb) Tail() (uint64, error) {
 	return 0, errNotSupported
 }
 
 // AncientSize returns an error as we don't have a backing chain freezer.
+// AncientSize 返回一个错误，因为我们没有支持链冻结器。
 func (db *nofreezedb) AncientSize(kind string) (uint64, error) {
 	return 0, errNotSupported
 }
 
 // ModifyAncients is not supported.
+// ModifyAncients 不支持。
 func (db *nofreezedb) ModifyAncients(func(ethdb.AncientWriteOp) error) (int64, error) {
 	return 0, errNotSupported
 }
 
 // TruncateHead returns an error as we don't have a backing chain freezer.
+// TruncateHead 返回一个错误，因为我们没有支持链冻结器。
 func (db *nofreezedb) TruncateHead(items uint64) (uint64, error) {
 	return 0, errNotSupported
 }
 
 // TruncateTail returns an error as we don't have a backing chain freezer.
+// TruncateTail 返回一个错误，因为我们没有支持链冻结器。
 func (db *nofreezedb) TruncateTail(items uint64) (uint64, error) {
 	return 0, errNotSupported
 }
 
 // Sync returns an error as we don't have a backing chain freezer.
+// Sync 返回一个错误，因为我们没有支持链冻结器。
 func (db *nofreezedb) Sync() error {
 	return errNotSupported
 }
@@ -144,36 +160,54 @@ func (db *nofreezedb) ReadAncients(fn func(reader ethdb.AncientReaderOp) error) 
 	// If we instead were to return errNotSupported here, then the caller would
 	// have to explicitly check for that, having an extra clause to do the
 	// non-ancient operations.
+	//
+	// ReadAncients 不像其他与古董相关的方法，此方法在调用时不返回 errNotSupported。
+	// 原因是调用者可能想做几件事：
+	// 1. 检查某些内容是否在冻结器中，
+	// 2. 如果不在，则检查 leveldb。
+	//
+	// 这将有效，因为 'fn' 内部的古董检查将返回错误，而 leveldb 的工作将继续。
+	//
+	// 如果我们在这里返回 errNotSupported，则调用者必须显式检查该错误，增加额外的非古董操作子句。
 	return fn(db)
 }
 
 // AncientDatadir returns an error as we don't have a backing chain freezer.
+// AncientDatadir 返回一个错误，因为我们没有支持链冻结器。
 func (db *nofreezedb) AncientDatadir() (string, error) {
 	return "", errNotSupported
 }
 
 // NewDatabase creates a high level database on top of a given key-value data
 // store without a freezer moving immutable chain segments into cold storage.
+// NewDatabase 在给定的键值数据存储上创建一个高级数据库，而不使用冻结器将不可变的链段移动到冷存储。
 func NewDatabase(db ethdb.KeyValueStore) ethdb.Database {
 	return &nofreezedb{KeyValueStore: db}
 }
 
 // resolveChainFreezerDir is a helper function which resolves the absolute path
 // of chain freezer by considering backward compatibility.
+// resolveChainFreezerDir 是一个辅助函数，通过考虑向后兼容性来解析链冻结器的绝对路径。
 func resolveChainFreezerDir(ancient string) string {
 	// Check if the chain freezer is already present in the specified
 	// sub folder, if not then two possibilities:
 	// - chain freezer is not initialized
 	// - chain freezer exists in legacy location (root ancient folder)
+	// 检查链冻结器是否已存在于指定的子文件夹中，如果不存在则有两种可能：
+	// - 链冻结器未初始化
+	// - 链冻结器存在于旧位置（根古董文件夹）
 	freezer := filepath.Join(ancient, ChainFreezerName)
 	if !common.FileExist(freezer) {
 		if !common.FileExist(ancient) {
 			// The entire ancient store is not initialized, still use the sub
 			// folder for initialization.
+			// 整个古董存储未初始化，仍然使用子文件夹进行初始化。
 		} else {
 			// Ancient root is already initialized, then we hold the assumption
 			// that chain freezer is also initialized and located in root folder.
 			// In this case fallback to legacy location.
+			// 古董根目录已初始化，我们假设链冻结器也已初始化并位于根文件夹中。
+			// 在这种情况下回退到旧位置。
 			freezer = ancient
 			log.Info("Found legacy ancient chain path", "location", ancient)
 		}
@@ -185,11 +219,12 @@ func resolveChainFreezerDir(ancient string) string {
 // value data store with a freezer moving immutable chain segments into cold
 // storage. The passed ancient indicates the path of root ancient directory
 // where the chain freezer can be opened.
-// TODO learn here
+// NewDatabaseWithFreezer 在给定的键值数据存储上创建一个高级数据库，并使用冻结器将不可变的链段移动到冷存储。传递的 ancient 指示链冻结器可以打开的根古董目录的路径。
 func NewDatabaseWithFreezer(db ethdb.KeyValueStore, ancient string, namespace string, readonly bool) (ethdb.Database, error) {
 	// Create the idle freezer instance. If the given ancient directory is empty,
 	// in-memory chain freezer is used (e.g. dev mode); otherwise the regular
 	// file-based freezer is created.
+	// 创建空闲冻结器实例。如果给定的古董目录为空，则使用内存中的链冻结器（例如开发模式）；否则创建常规的文件基础冻结器。
 	chainFreezerDir := ancient
 	if chainFreezerDir != "" {
 		chainFreezerDir = resolveChainFreezerDir(chainFreezerDir)
@@ -217,15 +252,27 @@ func NewDatabaseWithFreezer(db ethdb.KeyValueStore, ancient string, namespace st
 	//     upgrading to the freezer release, or we might have had a small chain and
 	//     not frozen anything yet. Ensure that no blocks are missing yet from the
 	//     key-value store, since that would mean we already had an old freezer.
+	// 由于冻结器可以与用户的键值数据库分开存储，用户请求冻结器和数据库的无效组合的可能性相当高。
+	// 确保我们不会因为提供冲突数据而自找麻烦，导致两个数据存储都被损坏。
+	//
+	//   - 如果冻结器和键值存储都为空（没有创世块），我们刚初始化了一个新的空冻结器，一切正常。
+	//   - 如果键值存储为空，但冻结器不为空，我们需要确保用户的创世块与冻结器匹配。这将在区块链中检查，
+	//     因为我们这里没有创世块（此时也不应该关心，键值/冻结器组合是有效的）。
+	//   - 如果键值存储和冻结器都不为空，交叉验证创世块哈希以确保它们兼容。如果兼容，还要确保冻结器和后续的 leveldb 之间没有间隙。
+	//   - 如果键值存储不为空，但冻结器为空，我们可能正在升级到冻结器版本，或者我们有一个小的链且尚未冻结任何东西。
+	//     确保键值存储中尚未缺少任何块，否则这意味着我们已经有一个旧的冻结器。
 
 	// If the genesis hash is empty, we have a new key-value store, so nothing to
 	// validate in this method. If, however, the genesis hash is not nil, compare
 	// it to the freezer content.
+	// 如果创世块哈希为空，我们有一个新的键值存储，因此此方法无需验证。
+	// 然而，如果创世块哈希不为空，则将其与冻结器内容进行比较。
 	if kvgenesis, _ := db.Get(headerHashKey(0)); len(kvgenesis) > 0 {
 		if frozen, _ := frdb.Ancients(); frozen > 0 {
 			// If the freezer already contains something, ensure that the genesis blocks
 			// match, otherwise we might mix up freezers across chains and destroy both
 			// the freezer and the key-value store.
+			// 如果冻结器已包含某些内容，确保创世块匹配，否则我们可能会混淆不同链的冻结器并毁坏冻结器和键值存储。
 			frgenesis, err := frdb.Ancient(ChainFreezerHashTable, 0)
 			if err != nil {
 				printChainMetadata(db)
@@ -236,12 +283,16 @@ func NewDatabaseWithFreezer(db ethdb.KeyValueStore, ancient string, namespace st
 			}
 			// Key-value store and freezer belong to the same network. Ensure that they
 			// are contiguous, otherwise we might end up with a non-functional freezer.
+			// 键值存储和冻结器属于同一网络。确保它们是连续的，否则我们可能会得到一个无法使用的冻结器。
 			if kvhash, _ := db.Get(headerHashKey(frozen)); len(kvhash) == 0 {
 				// Subsequent header after the freezer limit is missing from the database.
 				// Reject startup if the database has a more recent head.
+				// 冻结器限制后的后续头部在数据库中缺失。
+				// 如果数据库有更新的头部，则拒绝启动。
 				if head := *ReadHeaderNumber(db, ReadHeadHeaderHash(db)); head > frozen-1 {
 					// Find the smallest block stored in the key-value store
 					// in range of [frozen, head]
+					// 在 [frozen, head] 范围内找到键值存储中存储的最小块
 					var number uint64
 					for number = frozen; number <= head; number++ {
 						if present, _ := db.Has(headerHashKey(number)); present {
@@ -249,35 +300,45 @@ func NewDatabaseWithFreezer(db ethdb.KeyValueStore, ancient string, namespace st
 						}
 					}
 					// We are about to exit on error. Print database metadata before exiting
+					// 我们即将因错误退出。在退出前打印数据库元数据
 					printChainMetadata(db)
 					return nil, fmt.Errorf("gap in the chain between ancients [0 - #%d] and leveldb [#%d - #%d] ",
 						frozen-1, number, head)
 				}
 				// Database contains only older data than the freezer, this happens if the
 				// state was wiped and reinited from an existing freezer.
+				// 数据库仅包含比冻结器更旧的数据，如果状态被擦除并从现有冻结器重新初始化，则会发生这种情况。
 			}
 			// Otherwise, key-value store continues where the freezer left off, all is fine.
 			// We might have duplicate blocks (crash after freezer write but before key-value
 			// store deletion, but that's fine).
+			// 否则，键值存储从冻结器停止的地方继续，一切正常。
+			// 我们可能有重复的块（冻结器写入后但键值存储删除前崩溃，但这没问题）。
 		} else {
 			// If the freezer is empty, ensure nothing was moved yet from the key-value
 			// store, otherwise we'll end up missing data. We check block #1 to decide
 			// if we froze anything previously or not, but do take care of databases with
 			// only the genesis block.
+			// 如果冻结器为空，确保尚未从键值存储中移动任何内容，否则我们会丢失数据。
+			// 我们检查第 1 块以决定之前是否冻结了任何东西，但要注意只有创世块的数据库。
 			if ReadHeadHeaderHash(db) != common.BytesToHash(kvgenesis) {
 				// Key-value store contains more data than the genesis block, make sure we
 				// didn't freeze anything yet.
+				// 键值存储包含比创世块更多的数据，确保我们尚未冻结任何东西。
 				if kvblob, _ := db.Get(headerHashKey(1)); len(kvblob) == 0 {
 					printChainMetadata(db)
 					return nil, errors.New("ancient chain segments already extracted, please set --datadir.ancient to the correct path")
 				}
 				// Block #1 is still in the database, we're allowed to init a new freezer
+				// 第 1 块仍在数据库中，我们可以初始化一个新的冻结器
 			}
 			// Otherwise, the head header is still the genesis, we're allowed to init a new
 			// freezer.
+			// 否则，头部仍是创世块，我们可以初始化一个新的冻结器。
 		}
 	}
 	// Freezer is consistent with the key-value database, permit combining the two
+	// 冻结器与键值数据库一致，允许组合两者
 	if !readonly {
 		frdb.wg.Add(1)
 		go func() {
@@ -294,6 +355,7 @@ func NewDatabaseWithFreezer(db ethdb.KeyValueStore, ancient string, namespace st
 
 // NewMemoryDatabase creates an ephemeral in-memory key-value database without a
 // freezer moving immutable chain segments into cold storage.
+// NewMemoryDatabase 创建一个临时的内存键值数据库，而不使用冻结器将不可变的链段移动到冷存储。
 func NewMemoryDatabase() ethdb.Database {
 	return NewDatabase(memorydb.New())
 }
@@ -306,13 +368,14 @@ const (
 // PreexistingDatabase checks the given data directory whether a database is already
 // instantiated at that location, and if so, returns the type of database (or the
 // empty string).
+// PreexistingDatabase 检查给定的数据目录是否已经存在数据库实例，如果存在，则返回数据库的类型（否则返回空字符串）。
 func PreexistingDatabase(path string) string {
 	if _, err := os.Stat(filepath.Join(path, "CURRENT")); err != nil {
-		return "" // No pre-existing db
+		return "" // No pre-existing db  没有预先存在的数据库
 	}
 	if matches, err := filepath.Glob(filepath.Join(path, "OPTIONS*")); len(matches) > 0 || err != nil {
 		if err != nil {
-			panic(err) // only possible if the pattern is malformed
+			panic(err) // only possible if the pattern is malformed 仅在模式格式错误时可能发生
 		}
 		return DBPebble
 	}
@@ -351,6 +414,7 @@ func (s *stat) Count() string {
 
 // InspectDatabase traverses the entire database and checks the size
 // of all different categories of data.
+// InspectDatabase 遍历整个数据库并检查所有不同类别数据的大小。
 func InspectDatabase(db ethdb.Database, keyPrefix, keyStart []byte) error {
 	it := db.NewIterator(keyPrefix, keyStart)
 	defer it.Release()
@@ -360,7 +424,7 @@ func InspectDatabase(db ethdb.Database, keyPrefix, keyStart []byte) error {
 		start  = time.Now()
 		logged = time.Now()
 
-		// Key-value store statistics
+		// Key-value store statistics 键值存储统计
 		headers         stat
 		bodies          stat
 		receipts        stat
@@ -380,6 +444,8 @@ func InspectDatabase(db ethdb.Database, keyPrefix, keyStart []byte) error {
 		beaconHeaders   stat
 		cliqueSnaps     stat
 
+		// 其他统计变量
+
 		// Verkle statistics
 		verkleTries        stat
 		verkleStateLookups stat
@@ -395,7 +461,7 @@ func InspectDatabase(db ethdb.Database, keyPrefix, keyStart []byte) error {
 		// Totals
 		total common.StorageSize
 	)
-	// Inspect key-value database first.
+	// Inspect key-value database first. 首先检查键值数据库。
 	for it.Next() {
 		var (
 			key  = it.Key()
@@ -551,6 +617,7 @@ func InspectDatabase(db ethdb.Database, keyPrefix, keyStart []byte) error {
 }
 
 // printChainMetadata prints out chain metadata to stderr.
+// printChainMetadata 将链元数据打印到标准错误。
 func printChainMetadata(db ethdb.KeyValueStore) {
 	fmt.Fprintf(os.Stderr, "Chain metadata\n")
 	for _, v := range ReadChainMetadata(db) {
@@ -562,6 +629,7 @@ func printChainMetadata(db ethdb.KeyValueStore) {
 // ReadChainMetadata returns a set of key/value pairs that contains information
 // about the database chain status. This can be used for diagnostic purposes
 // when investigating the state of the node.
+// ReadChainMetadata 返回一组键/值对，包含有关数据库链状态的信息。这可用于诊断目的，调查节点的状态。
 func ReadChainMetadata(db ethdb.KeyValueStore) [][]string {
 	pp := func(val *uint64) string {
 		if val == nil {
