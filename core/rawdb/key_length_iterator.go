7577f4438efaeac9110d9a21007884b6141064c8
sunyihoo <sunyihoo@gmail.com>
2025-04-02 23:30:40 +0800

    notes & test: core rawdb.key_length_iterator

--- core/rawdb/key_length_iterator.go
+++ core/rawdb/key_length_iterator.go
@@ -20,22 +20,26 @@ import "github.com/ethereum/go-ethereum/ethdb"
 
 // KeyLengthIterator is a wrapper for a database iterator that ensures only key-value pairs
 // with a specific key length will be returned.
+// KeyLengthIterator 是一个数据库迭代器的包装器，用于确保只返回具有特定键长度的键值对。
 type KeyLengthIterator struct {
-	requiredKeyLength int
-	ethdb.Iterator
+	requiredKeyLength int // 所需键的长度。
+	ethdb.Iterator        // 底层迭代器。
 }
 
 // NewKeyLengthIterator returns a wrapped version of the iterator that will only return key-value
 // pairs where keys with a specific key length will be returned.
+// NewKeyLengthIterator 返回一个包装后的迭代器，该迭代器仅返回键长度符合指定长度的键值对。
 func NewKeyLengthIterator(it ethdb.Iterator, keyLen int) ethdb.Iterator {
 	return &KeyLengthIterator{
-		Iterator:          it,
-		requiredKeyLength: keyLen,
+		Iterator:          it,     // 被包装的底层迭代器。
+		requiredKeyLength: keyLen, // 指定的键长度。
 	}
 }
 
+// Next 方法通过底层迭代器的 Next() 方法逐步检查每一个键的长度。
 func (it *KeyLengthIterator) Next() bool {
 	// Return true as soon as a key with the required key length is discovered
+	// 一旦发现符合所需键长度的键，就返回 true。
 	for it.Iterator.Next() {
 		if len(it.Iterator.Key()) == it.requiredKeyLength {
 			return true
@@ -43,5 +47,6 @@ func (it *KeyLengthIterator) Next() bool {
 	}
 
 	// Return false when we exhaust the keys in the underlying iterator.
+	// 如果在底层迭代器中耗尽了键，则返回 false。
 	return false
 }
