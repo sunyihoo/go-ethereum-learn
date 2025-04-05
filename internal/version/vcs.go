db98c63ce0da31ae8a4e3b5d580e2321a42115cd
sunyihoo <sunyihoo@gmail.com>
2025-04-05 16:57:59 +0800

    notes: internal version

--- internal/version/vcs.go
+++ internal/version/vcs.go
@@ -22,27 +22,31 @@ import (
 )
 
 // In go 1.18 and beyond, the go tool embeds VCS information into the build.
-
+// 在 Go 1.18 及以上版本中，go 工具会在构建时嵌入 VCS 信息。
 const (
-	govcsTimeLayout = "2006-01-02T15:04:05Z"
-	ourTimeLayout   = "20060102"
+	govcsTimeLayout = "2006-01-02T15:04:05Z" // Go VCS 时间格式，遵循 ISO 8601。
+	ourTimeLayout   = "20060102"             // 自定义时间格式，简化为 YYYYMMDD。
 )
 
 // These variables are set at build-time by the linker when the build is
 // done by build/ci.go.
+// 这些变量在构建时由链接器设置，通常通过 build/ci.go 完成。
 var gitCommit, gitDate string
 
 // VCSInfo represents the git repository state.
+// VCSInfo 表示 git 仓库的状态。
 type VCSInfo struct {
-	Commit string // head commit hash
-	Date   string // commit time in YYYYMMDD format
-	Dirty  bool
+	Commit string // head commit hash 头部提交哈希。
+	Date   string // commit time in YYYYMMDD format 提交时间，格式为 YYYYMMDD。
+	Dirty  bool   // 是否有未提交的更改。
 }
 
 // VCS returns version control information of the current executable.
+// VCS 返回当前可执行文件的版本控制信息。
 func VCS() (VCSInfo, bool) {
 	if gitCommit != "" {
 		// Use information set by the build script if present.
+		// 如果存在构建脚本设置的信息，则使用它。
 		return VCSInfo{Commit: gitCommit, Date: gitDate}, true
 	}
 	if buildInfo, ok := debug.ReadBuildInfo(); ok {
@@ -54,24 +58,25 @@ func VCS() (VCSInfo, bool) {
 }
 
 // buildInfoVCS returns VCS information of the build.
+// buildInfoVCS 返回构建的 VCS 信息。
 func buildInfoVCS(info *debug.BuildInfo) (s VCSInfo, ok bool) {
 	for _, v := range info.Settings {
 		switch v.Key {
 		case "vcs.revision":
-			s.Commit = v.Value
+			s.Commit = v.Value // 设置提交哈希。
 		case "vcs.modified":
 			if v.Value == "true" {
-				s.Dirty = true
+				s.Dirty = true // 如果值为 "true"，表示仓库有未提交的更改。
 			}
 		case "vcs.time":
 			t, err := time.Parse(govcsTimeLayout, v.Value)
 			if err == nil {
-				s.Date = t.Format(ourTimeLayout)
+				s.Date = t.Format(ourTimeLayout) // 将 VCS 时间转换为自定义格式。
 			}
 		}
 	}
 	if s.Commit != "" && s.Date != "" {
-		ok = true
+		ok = true // 如果提交哈希和日期都存在，则返回成功。
 	}
 	return
 }
--- internal/version/version.go
+++ internal/version/version.go
@@ -27,14 +27,18 @@ import (
 )
 
 const ourPath = "github.com/ethereum/go-ethereum" // Path to our module
+// 定义我们模块的路径，即 go-ethereum 的 GitHub 地址。
 
 // Family holds the textual version string for major.minor
+// Family 保存主版本号和小版本号的文本版本字符串。
 var Family = fmt.Sprintf("%d.%d", version.Major, version.Minor)
 
 // Semantic holds the textual version string for major.minor.patch.
+// Semantic 保存主版本号、小版本号和补丁号的文本版本字符串。
 var Semantic = fmt.Sprintf("%d.%d.%d", version.Major, version.Minor, version.Patch)
 
 // WithMeta holds the textual version string including the metadata.
+// WithMeta 保存包含元数据的文本版本字符串。
 var WithMeta = func() string {
 	v := Semantic
 	if version.Meta != "" {
@@ -44,6 +48,7 @@ var WithMeta = func() string {
 }()
 
 func WithCommit(gitCommit, gitDate string) string {
+	// WithCommit 返回包含 git 提交哈希和日期的版本字符串。
 	vsn := WithMeta
 	if len(gitCommit) >= 8 {
 		vsn += "-" + gitCommit[:8]
@@ -57,6 +62,7 @@ func WithCommit(gitCommit, gitDate string) string {
 // Archive holds the textual version string used for Geth archives. e.g.
 // "1.8.11-dea1ce05" for stable releases, or "1.8.13-unstable-21c059b6" for unstable
 // releases.
+// Archive 保存用于 Geth 存档的文本版本字符串。例如，稳定版本为 "1.8.11-dea1ce05"，不稳定版本为 "1.8.13-unstable-21c059b6"。
 func Archive(gitCommit string) string {
 	vsn := Semantic
 	if version.Meta != "stable" {
@@ -70,6 +76,7 @@ func Archive(gitCommit string) string {
 
 // ClientName creates a software name/version identifier according to common
 // conventions in the Ethereum p2p network.
+// ClientName 根据以太坊 P2P 网络的常见惯例创建软件名称/版本标识符。
 func ClientName(clientIdentifier string) string {
 	git, _ := VCS()
 	return fmt.Sprintf("%s/v%v/%v-%v/%v",
@@ -86,6 +93,8 @@ func ClientName(clientIdentifier string) string {
 // module path, it will print out commit and date VCS information. Otherwise,
 // it will assume it's imported by a third-party and will return the imported
 // version and whether it was replaced by another module.
+// Info 返回当前二进制文件的构建和平台信息。
+// 如果当前执行的包以 go-ethereum 模块路径为前缀，它将输出提交和日期的 VCS 信息。否则，它将假设被第三方导入，并返回导入的版本以及是否被另一个模块替换。
 func Info() (version, vcs string) {
 	version = WithMeta
 	buildInfo, ok := debug.ReadBuildInfo()
@@ -113,16 +122,21 @@ func Info() (version, vcs string) {
 // Depending on how the code is instantiated, it returns different amounts of
 // information. If it is unable to determine which module is related to our
 // package it falls back to the hardcoded values in the params package.
+// versionInfo 返回当前执行实现的版本信息。
+// 根据代码的实例化方式，它返回不同数量的信息。如果无法确定与我们包相关的模块，它将回退到 params 包中的硬编码值。
 func versionInfo(info *debug.BuildInfo) string {
 	// If the main package is from our repo, prefix version with "geth".
+	// 如果主包来自我们的仓库，则版本前缀为 "geth"。
 	if strings.HasPrefix(info.Path, ourPath) {
 		return fmt.Sprintf("geth %s", info.Main.Version)
 	}
 	// Not our main package, so explicitly print out the module path and
 	// version.
+	// 不是我们的主包，因此明确打印出模块路径和版本。
 	var version string
 	if info.Main.Path != "" && info.Main.Version != "" {
 		// These can be empty when invoked with "go run".
+		// 当使用 "go run" 调用时，这些可能为空。
 		version = fmt.Sprintf("%s@%s ", info.Main.Path, info.Main.Version)
 	}
 	mod := findModule(info, ourPath)
@@ -130,19 +144,23 @@ func versionInfo(info *debug.BuildInfo) string {
 		// If our module path wasn't imported, it's unclear which
 		// version of our code they are running. Fallback to hardcoded
 		// version.
+		// 如果我们的模块路径未被导入，则不清楚他们运行的是哪个版本的代码。回退到硬编码版本。
 		return version + fmt.Sprintf("geth %s", WithMeta)
 	}
 	// Our package is a dependency for the main module. Return path and
 	// version data for both.
+	// 我们的包是主模块的依赖项。返回两者的路径和版本数据。
 	version += fmt.Sprintf("%s@%s", mod.Path, mod.Version)
 	if mod.Replace != nil {
 		// If our package was replaced by something else, also note that.
+		// 如果我们的包被其他东西替换，也记录下来。
 		version += fmt.Sprintf(" (replaced by %s@%s)", mod.Replace.Path, mod.Replace.Version)
 	}
 	return version
 }
 
 // findModule returns the module at path.
+// findModule 返回指定路径的模块。
 func findModule(info *debug.BuildInfo, path string) *debug.Module {
 	if info.Path == ourPath {
 		return &info.Main
